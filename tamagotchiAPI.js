5feea2d4075d259bc5ab2ac3e8552410136af031
psikoo <hugohernandezmuelas03@gmail.com>
2024-10-07 22:21:54 +0200

    added play, feed, rest

--- tamagotchiAPI.js
+++ tamagotchiAPI.js
@@ -15,11 +15,24 @@ app.get("/getTamagotchi", async (req, res) => {
     res.json(tamagotchi);
 });
 
+app.get("/play", async (req, res) => {
+    if(tamagotchi.happiness < 10) { tamagotchi.happiness++; tamagotchi.energy--; }
+    res.json(tamagotchi);
+});
+app.get("/feed", async (req, res) => {
+    if(tamagotchi.happiness < 10) { tamagotchi.hunger++; tamagotchi.energy++; }
+    res.json(tamagotchi);
+});
+app.get("/rest", async (req, res) => {
+    if(tamagotchi.happiness < 10) { tamagotchi.energy++; tamagotchi.hunger--; }
+    res.json(tamagotchi);
+});
+
 app.post("/reload", (req, res) => {
     let timePassed = (new Date().valueOf())-tamagotchi.lastUpdate;
     let secondsPassed = timePassed/1000;
-    let timeForTick = 10;
-    if(secondsPassed/timeForTick >= 1) { // change to 1h 
+    let timeForTick = 10; // change to 3600
+    if(secondsPassed/timeForTick >= 1) { 
         let subtract = Math.floor(secondsPassed/timeForTick);
         if((tamagotchi.happiness - subtract <= 0) || (tamagotchi.hunger - subtract <= 0) || (tamagotchi.energy - subtract <= 0)) {
             tamagotchi.state = "Dead";
